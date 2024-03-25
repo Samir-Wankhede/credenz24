6d8a1812114de9727411cfd36d7baf182e8391f1
Samir-Wankhede <137442566+Samir-Wankhede@users.noreply.github.com>
2024-03-25 19:26:16 +0530

    updated working

--- src/models/Submarine.jsx
+++ src/models/Submarine.jsx
@@ -7,11 +7,14 @@ import gsap from "gsap"
 
 export default function Model(props) {
   const [isSinking, setIsSinking] = useState(true);
-  const { nodes, materials } = useGLTF('/models/credenz_baked.glb')
+  //const { nodes, materials } = useGLTF('/models/credenz_baked.glb')
+  const model=useGLTF('/models/onlySub.glb')
   const [rigSpeed, setRigSpeed]=useState(2.5)
   const ref=useRef()
   const grpRef = useRef()
   useEffect(() => {
+    //droping animation is only when use effect fires
+    console.log("hereinsub")
     const timeout = setTimeout(() => {
         setIsSinking(false) 
         setRigSpeed(1.5);
@@ -23,19 +26,19 @@ export default function Model(props) {
 function InitialSink (){
     useFrame((state, delta) => {
         isSinking&&gsap.to(ref.current.position,{y:0,duration:rigSpeed})
-        isSinking&&gsap.to(ref.current.rotation,{y:-Math.PI/2 + 0.5,duration:rigSpeed}) 
+        isSinking&&gsap.to(ref.current.rotation,{y:0.5,duration:rigSpeed}) 
     })
   }
   function UnderWaterMove(){
     useFrame((state, delta) => {
-         !isSinking&&ref.current.lookAt(((state.mouse.y)*(state.viewport.height))/2,((state.mouse.x)*(state.viewport.width))/2,0)
+         !isSinking&&ref.current.lookAt(((state.mouse.x)*(state.viewport.width))/2,((state.mouse.y)*(state.viewport.height))/2,1)
          !isSinking&&gsap.to(ref.current.position,{y:((state.mouse.y)*(state.viewport.height))/2,x:((state.mouse.x)*(state.viewport.width))/2,duration:rigSpeed,delay:0.01})
     })
     return null
   }
   return (
     <>
-    <group {...props} dispose={null}>
+    {/* <group {...props} dispose={null}>
         <mesh 
         geometry={nodes.Sub.geometry} 
         material={nodes.Sub.material} 
@@ -44,10 +47,16 @@ function InitialSink (){
         rotation={[Math.PI/2,0,-Math.PI/2]}
         ref={ref}
         />
-    </group>
+
+    </group> */}
+    <primitive ref={ref} object={model.scene} dispose={null}
+    scale={[0.15,0.15,0.15]}
+    position={[0,20,0]}
+    rotation={[0,Math.PI/2,0]}
+    />
     <InitialSink />
     <UnderWaterMove/>
     </>
   )
 }
-useGLTF.preload('/models/credenz_baked.glb')
+useGLTF.preload('/models/onlySub.glb')
