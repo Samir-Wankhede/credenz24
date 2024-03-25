c30a57a681fb1a6b40af6cee378dbc934413b6f2
Samir-Wankhede <137442566+Samir-Wankhede@users.noreply.github.com>
2024-03-25 16:44:08 +0530

    underWater Part without up going option
    to go up put functionality in underWater.jsx

---
+++ src/pages/underwater/underWater.jsx
@@ -0,0 +1,37 @@
+import { Environment, useProgress } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense, useEffect, useState } from 'react'
+import Experience from './Experience'
+import LoadPage from '../loading/LoadPage'
+import './Experience.css'
+
+export default function Landing() {
+    const [explore,setExplore]=useState(false)
+    const [showToggle,setShowToggle]=useState(false)
+    const {progress}=useProgress()
+
+    useEffect(()=>{
+
+    },[])
+
+  return (
+    <>
+    {showToggle && 
+        <div className='toggle-div'>
+            <div className='container'>
+                <input type='checkbox' name='checkbox' value='checkbox' 
+                checked={explore} id='checkbox'
+                onChange={(e)=>setExplore(e.target.checked)}/>
+                <label className="label" htmlFor="checkbox" ></label>
+
+                
+            </div>
+            <div>Return</div>
+        </div>
+    }
+    <Canvas className='canvas' camera={{ position: [0, 0, 20] }}>
+        <Experience  />
+    </Canvas>
+  </>
+  )
+}
